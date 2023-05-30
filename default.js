0d2acacc4f59963ba5e5c7c89419df5798d95cf3
Yeseol Lee <ashleylee9404@gmail.com>
2023-05-30 14:42:53 +0900

    operator

--- default.js
+++ default.js
@@ -1,27 +1,50 @@
-var num = "20";
-num = num.toString();
-console.log(typeof num);
 
-var number = 20;
-console.log(typeof number);
+//input으로 두 숫자 받기
+var num1 = parseInt(prompt("Enter first number: "));
+var num2 = parseInt(prompt("Enter second number: "));
+var lineBreak = "<br/>"
+//덧셈
+var result = num1+num2;
+document.write("The sum is: "+ result+ lineBreak);
+//뺄셈
+var result = num1-num2;
+document.write("The sub is: "+ result+ lineBreak);
+//곱셈
+var result = num1*num2;
+document.write("The mult is: "+ result+ lineBreak);
+//나누기
+var result = num1 / num2;
+document.write("The division is: "+ result.toPrecision(3)+ lineBreak);
+//나머지
+var result = num1 %num2;
+document.write("The remainder is: "+ result+ lineBreak);
 
-number = number.toString();
-console.log(number, typeof number);
 
-var x = 2.5678;
-//toFixed - 숫자를 고정 소수점 표기법으로 표기해 반환합니다. 
-console.log(x.toFixed(1), typeof x.toFixed(1)); // 2.6 string
-console.log(x.toFixed(2)); //2.57 srting
 
-//toPrecision- 고정 소수점 또는 지수 표기법의 수를 정밀 유효 숫자로 반올림 한 문자열을 반환합니다. 
-console.log(x.toPrecision(1), typeof x.toPrecision(1));  // 3 string
-console.log(x.toPrecision(2)); //2.6
+// var num = "20";
+// num = num.toString();
+// console.log(typeof num);
 
-console.log(Number(true));
-console.log(Number(false) );
-console.log(Number("10"));
-console.log(Number(" 10 "));
-console.log(Number("10.25"));
+// var number = 20;
+// console.log(typeof number);
+
+// number = number.toString();
+// console.log(number, typeof number);
+
+// var x = 2.5678;
+// //toFixed - 숫자를 고정 소수점 표기법으로 표기해 반환합니다. 
+// console.log(x.toFixed(1), typeof x.toFixed(1)); // 2.6 string
+// console.log(x.toFixed(2)); //2.57 srting
+
+// //toPrecision- 고정 소수점 또는 지수 표기법의 수를 정밀 유효 숫자로 반올림 한 문자열을 반환합니다. 
+// console.log(x.toPrecision(1), typeof x.toPrecision(1));  // 3 string
+// console.log(x.toPrecision(2)); //2.6
+
+// console.log(Number(true));
+// console.log(Number(false) );
+// console.log(Number("10"));
+// console.log(Number(" 10 "));
+// console.log(Number("10.25"));
 // var text = prompt("Enter your name: ");
 // document.write("Your name: "+text +"<br/>");
 
