d0b3551efbf9e0f73b43a5c8ac1f5e13aafe0808
Yeseol Lee <ashleylee9404@gmail.com>
2023-05-30 13:03:34 +0900

    var typeof

--- default.js
+++ default.js
@@ -1,13 +1,31 @@
-document.write("Hello world");
-document.write("<h1>Welcome to JS Program</h1>");
-document.write("<h2>Welcome to JS Program</h2>");
+
+// console.log(typeof 123);
+// console.log(typeof 123.5);
+// console.log(typeof "123");
+// console.log(typeof true);
+// console.log(typeof false);
+
+var car;
+console.log(car);
+var car ="";
+console.log(typeof car);
+var person ={firstName:"John", lastName:"Doe", age:50, eyeColor:'blue'};
+console.log(typeof person);
+person= null;
+
+console.log(typeof person);
 
 
-console.log("Welcome JS Program");
-console.info( "Welcome JS Program");
-console.warn( "Welcome JS Program");
-console.error("Welcome JS Program");
+// document.write("Hello world");
+// document.write("<h1>Welcome to JS Program</h1>");
+// document.write("<h2>Welcome to JS Program</h2>");
 
-alert('Welcome JS Program');
-var a = prompt("Welcome JS Program");
-console.log(a)
+// console.log("Welcome JS Program");
+// console.info( "Welcome JS Program");
+// console.warn( "Welcome JS Program");
+// console.error("Welcome JS Program");
+
+// alert('Welcome JS Program');
+// var a = prompt("Welcome JS Program");
+// console.log(a);
+
