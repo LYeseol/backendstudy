1ac0b1c888b0cf848e968e92808fd2ca2efbddb5
Yeseol Lee <ashleylee9404@gmail.com>
2023-05-31 11:13:57 +0900

    switch 구문

--- default.js
+++ default.js
@@ -1,14 +1,54 @@
 
-//모음자음 구분하기
-var letter = prompt("Enter a letter: ");
+var digit = parseInt(prompt("Enter a digit: "));
 
-letter =letter.toLowerCase();
+switch(digit){
+    case 0:
+        document.write("Zero");
+        break;
+    case 1:
+        document.write("One");
+        break;    
+    case 2:
+        document.write("Two");
+        break;
+    case 3:
+        document.write("Three");
+        break;
+    case 4:
+        document.write("Four");
+        break;
+    case 5:
+        document.write("Five");
+        break;    
+    case 6:
+        document.write("Six");
+        break;
+    case 7:
+        document.write("Seven");
+        break;
 
-if(letter == 'a'|| letter=='e' || letter=='i'|| letter =='o'|| letter =='u'){
-    console.log('Vowel');
-}else{
-    console.log('Consonant');
-}
+    case 8:
+        document.write("Eight");
+        break;
+    case 9:
+        document.write("Nine");
+        break;    
+    default:
+        document.write("Not a Digit");
+    }
+
+
+
+// //모음자음 구분하기
+// var letter = prompt("Enter a letter: ");
+
+// letter =letter.toLowerCase();
+
+// if(letter == 'a'|| letter=='e' || letter=='i'|| letter =='o'|| letter =='u'){
+//     console.log('Vowel');
+// }else{
+//     console.log('Consonant');
+// }
 
 
 
