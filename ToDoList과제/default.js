def278457220b8def7142109ec174fc2addff5c3
Yeseol Lee <ashleylee9404@gmail.com>
2023-06-02 02:16:16 +0900

    Assignment -To do list

---
+++ ToDoList과제/default.js
@@ -0,0 +1,45 @@
+var toDoList=[];
+// get input and save it into list
+function addList(){
+    var results = document.getElementById('finalResult');
+    var newone= document.getElementById('list').value;
+    var item = document.createElement('li');
+    //no input
+    if(newone==''){
+        alert("Add todo list");
+    }
+    else{
+       
+        item.textContent = newone;
+        item.classList.add('list-group-item','list-group-item-secondary',"form-check");
+        
+        results.appendChild(item);
+        toDoList.push(newone);
+        // add remove button 
+        var removebtn = document.createElement('button');
+        removebtn.innerText = 'Remove';
+        removebtn.classList.add('btn', 'btn-outline-danger');
+        removebtn.addEventListener('click', removeList);
+        item.appendChild(removebtn);
+                        
+    }
+    
+}
+
+function removeList(){
+    var results= document.getElementById('finalResult');
+    var t = this.parentNode.textContent; 
+    t=t.substr(0,t.length-2);
+    results.removeChild(this.parentNode);
+    for (var i =0; i< toDoList.length; i++){
+        if (toDoList[i] == t) {
+            toDoList.splice(i,1);
+        }
+    }
+    
+}
+
+function clearList(){
+    var results=document.getElementById('finalResult');
+    results.innerHTML='';          
+}
