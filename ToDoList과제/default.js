adf839a6fd0d43d565641c68dadd685885e8403a
Yeseol Lee <ashleylee9404@gmail.com>
2023-06-02 11:04:12 +0900

    change removelist

--- ToDoList과제/default.js
+++ ToDoList과제/default.js
@@ -9,12 +9,13 @@ function addList(){
         alert("Add todo list");
     }
     else{
-       
+       //add todo list
         item.textContent = newone;
         item.classList.add('list-group-item','list-group-item-secondary',"form-check");
         
         results.appendChild(item);
         toDoList.push(newone);
+        console.log(toDoList);
         // add remove button 
         var removebtn = document.createElement('button');
         removebtn.innerText = 'Remove';
@@ -23,19 +24,17 @@ function addList(){
         item.appendChild(removebtn);
                         
     }
-
 }
-
+//remove specific element from the list
 function removeList(){
     var results= document.getElementById('finalResult');
-    var t = this.parentElement.innerText;
     results.removeChild(this.parentNode);
-    console.log("before",toDoList)
+//    remove element in todoList by index
     var idx = toDoList.indexOf(this.parentElement.innerText.split("Remove")[0])
-    toDoList.splice(idx,1)
-    console.log(toDoList)
+    toDoList.splice(idx,1);
+    console.log(toDoList);
 }
-
+//clear all
 function clearList(){
     var results=document.getElementById('finalResult');
     results.innerHTML='';
