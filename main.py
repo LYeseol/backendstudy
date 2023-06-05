b0c6651ff7de0cf5ba0fbe51cfad6badd543d199
Yeseol Lee <ashleylee9404@gmail.com>
2023-06-05 13:25:32 +0900

    비트연산자

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-a=5
-b=2
-c=3
-d=200
+#비트연산자 실습
+a=10
+b=20
 
-print('AND 연산자')
-print('a:%d  b: %d' %(a,b) )
-print('a>b and a<c: ', a>b and a>c)
-print('a>b and a<c: ', a>b and a<c)
-
-print('OR연산자')
-print('a>b and a<c: ', a>b or a>c)
-print('a>b and a<c: ', a>b or a<c)
-
-print('NOT 연산자')
-print('a:%d  b: %d c:%d' %(a,b,c) )
-print('not(a<b): ', not(a<b) )
-print('not(a<c): ', not(a<c))
+print('a: ',bin(a))
+print('b: ',bin(b))
