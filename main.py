e2148179595843ee77d9f044416c62790fc9b434
Yeseol Lee <ashleylee9404@gmail.com>
2023-06-05 10:23:58 +0900

    python practice

---
+++ main.py
@@ -0,0 +1,16 @@
+a=10
+b=3.14
+c="Hello World!"
+d=[1,2,3]
+e= (4,5,6)
+f={7,8,9}
+g={"apple":1, "banna":2, "oragne":3}
+
+
+print("type a: ", type(a))
+print("type b: ",type(b))
+print("type c: ",type(c))
+print("type d: ",type(d))
+print("type e: ",type(e))
+print("type f: ",type(f))
+print("type g: ",type(g))
