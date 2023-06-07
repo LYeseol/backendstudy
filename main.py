8758a4d6cf4118a7f78a03f5f0315e5bef592556
Yeseol Lee <ashleylee9404@gmail.com>
2023-06-07 09:33:05 +0900

    input-if

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-#비트연산자 실습
-a=10
-b=20
-
-print('a: ',bin(a))
-print('b: ',bin(b))
+day = input("요일을 입력하세요: ")
+if day == "mon":
+    print("monday")
+elif day=='tue':
+    print("Tuesday")
+elif day=='wed':
+    print("Wednesday")
