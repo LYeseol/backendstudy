044061f763fce35e1a6138ff2198015cfa9a322e
Yeseol Lee <ashleylee9404@gmail.com>
2023-06-07 15:25:45 +0900

    function

--- main.py
+++ main.py
@@ -1,69 +1,27 @@
-from array import *
-
-print('parameter가 없는 함수')
-def disp():
-    name ='lion'
-    print('welcome to ', name)
-
+#
+# def add():
+#     x = 10
+#     y = 20
+#     c = x+y
+#     return c
+#
+# sum = add()
+# print(sum)
+#
+# def add2():
+#     x=10
+#     y=20
+#
+#     return x+y
+#
+# sum2 =add2()
+# print(sum2)
 
 def add(y):
-    x=10
+    x =10
     c=x+y
-    print(c)
-    print(f'formatted output {x+y: }')
+    d = y-x
+    return c, d, 50
 
-add(20)
-
-
-#
-# stu_roll = array('i', [101,102,103,104,105,106,107])
-# print('배열 슬라이싱')
-# n= len(stu_roll)
-# for i in range(n):
-#     print(i,'= ', stu_roll[i])
-#
-# a = stu_roll[1:5]
-# for i in a:
-#     print(i)
-#
-# print('5까지')
-# a = stu_roll[:5]
-# for i in a:
-#     print(i)
-#
-# print('마지막 4개')
-# a = stu_roll[-4:]
-# for i in a:
-#     print(i)
-# print('마지막 1개 빼고')
-# a = stu_roll[:3]
-# for i in a:
-#     print(i)
-
-# print('remove after pop()')
-# stu_roll.pop()
-# n = len(stu_roll)
-# i=0
-# while i< n:
-#     print(stu_roll[i])
-#     i+=1
-
-# while i<n:
-#     print(stu_roll[i])
-#     i+=1
-#
-# print("array after insert")
-# stu_roll.insert(1, 106)
-# stu_roll.insert(3, 107)
-# n = len(stu_roll)
-# i = 0
-# while i<n:
-#     print(stu_roll[i])
-#     i+=1
-#
-# print('remove after')
-# stu_roll.remove(101)
-# i=0
-# while i< len(stu_roll):
-#     print(stu_roll[i])
-#     i+=1
+sum4, sub, a = add(20)
+print(sum4, ' ',  sub, ' ')
