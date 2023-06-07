9af6076c1cdb47f367e67bd59dfff1d8f31dffdf
Yeseol Lee <ashleylee9404@gmail.com>
2023-06-07 15:12:28 +0900

    function

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 from array import *
 
-stu_roll = array('i', [101,102,103,104,105,106,107])
-print('배열 슬라이싱')
-n= len(stu_roll)
-for i in range(n):
-    print(i,'= ', stu_roll[i])
+print('parameter가 없는 함수')
+def disp():
+    name ='lion'
+    print('welcome to ', name)
 
-a = stu_roll[1:5]
-for i in a:
-    print(i)
 
-print('5까지')
-a = stu_roll[:5]
-for i in a:
-    print(i)
+def add(y):
+    x=10
+    c=x+y
+    print(c)
+    print(f'formatted output {x+y: }')
 
-print('마지막 4개')
-a = stu_roll[-4:]
-for i in a:
-    print(i)
-print('마지막 1개 빼고')
-a = stu_roll[:3]
-for i in a:
-    print(i)
+add(20)
+
+
+#
+# stu_roll = array('i', [101,102,103,104,105,106,107])
+# print('배열 슬라이싱')
+# n= len(stu_roll)
+# for i in range(n):
+#     print(i,'= ', stu_roll[i])
+#
+# a = stu_roll[1:5]
+# for i in a:
+#     print(i)
+#
+# print('5까지')
+# a = stu_roll[:5]
+# for i in a:
+#     print(i)
+#
+# print('마지막 4개')
+# a = stu_roll[-4:]
+# for i in a:
+#     print(i)
+# print('마지막 1개 빼고')
+# a = stu_roll[:3]
+# for i in a:
+#     print(i)
 
 # print('remove after pop()')
 # stu_roll.pop()
