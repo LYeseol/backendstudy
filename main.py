58145d56791dc6a09d55c999830ec206519f41fa
Yeseol Lee <ashleylee9404@gmail.com>
2023-06-07 13:29:35 +0900

    array

--- main.py
+++ main.py
@@ -1,9 +1,53 @@
-import array
-stu_roll= array.array('i', [101,102,103,104])
+from array import *
 
-for i in stu_roll:
+stu_roll = array('i', [101,102,103,104,105,106,107])
+print('배열 슬라이싱')
+n= len(stu_roll)
+for i in range(n):
+    print(i,'= ', stu_roll[i])
+
+a = stu_roll[1:5]
+for i in a:
     print(i)
 
-n = len(stu_roll)
-for i in range(n):
-    print(i , ' = ' ,stu_roll[i])
+print('5까지')
+a = stu_roll[:5]
+for i in a:
+    print(i)
+
+print('마지막 4개')
+a = stu_roll[-4:]
+for i in a:
+    print(i)
+print('마지막 1개 빼고')
+a = stu_roll[:3]
+for i in a:
+    print(i)
+
+# print('remove after pop()')
+# stu_roll.pop()
+# n = len(stu_roll)
+# i=0
+# while i< n:
+#     print(stu_roll[i])
+#     i+=1
+
+# while i<n:
+#     print(stu_roll[i])
+#     i+=1
+#
+# print("array after insert")
+# stu_roll.insert(1, 106)
+# stu_roll.insert(3, 107)
+# n = len(stu_roll)
+# i = 0
+# while i<n:
+#     print(stu_roll[i])
+#     i+=1
+#
+# print('remove after')
+# stu_roll.remove(101)
+# i=0
+# while i< len(stu_roll):
+#     print(stu_roll[i])
+#     i+=1
