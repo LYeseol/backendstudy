408f5402728e31fcc9d020c82836e2591abde789
Yeseol Lee <ashleylee9404@gmail.com>
2023-06-09 10:34:07 +0900

    class 메소드 오버로딩/오버라이딩

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
-class Mobile:
-    fp ='yes' #class member 변수
+class Vector(object):
+    def __init__(self,x,y):
+        #instance variable
+        self.x = x
+        self.y = y
 
-realme = Mobile()
-redme = Mobile()
-geek = Mobile()
+    #연산자 오버로딩- 클래스 내에서 같은 이름의 메소드를 여러 개 선언, 상속 관련 x
+    def __add__(self, other):
+        return Vector(self.x+other.x, self.y+other.y)
+    #연산자 오버라이딩==> 기존 메소드 변경
+    def __str__(self):
+        return f'Vector({self.x}, {self.y})'
 
-#class name space
-print(Mobile.fp)
-print(realme.fp)
-print(redme.fp)
-print(geek.fp)
+a = Vector(1,2)
+b = Vector(3,4)
 
-Mobile.fp = 'no'
-print(Mobile.fp)
-print(realme.fp)
-print(redme.fp)
-print(geek.fp)
-
-realme.fp ='Not Working'
-print(Mobile.fp)
-print(realme.fp)
-print(redme.fp)
-print(geek.fp)
+print(a)
+print(b)
+print(a+b)
