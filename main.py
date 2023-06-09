b832967281d063b6d08373b88f65fd51e19147c3
Yeseol Lee <ashleylee9404@gmail.com>
2023-06-09 11:25:39 +0900

    상속

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-import time
-# print(time.time())
-# print(time.ctime())
+class ParentClass:
+    def __init__(self):
+        self.name = 'parent'
+        self.number = 10
 
-from datetime import datetime
-from datetime import date
+    def __str__(self):
+        return f'ParentClass name: {self.name}, numer:{self.number}'
+    def add_num(self,new_number):
+        print('부모: ', new_number, '만큼 더 해야지')
+        self.number = self.number + new_number
+class ChildClass(ParentClass):
+    def __init__(self):
+        super().__init__()
+        self.name='child'
+    def __str__(self):
+        return f'Childclass name: {self.name}, number: {self.number}'
+    def add_num(self,new_number):
+        print('고정적으로 5 더할건데?')
+        self.number = self.number +5
 
-dt = datetime(year=2023, month=5, day = 5, hour=10, minute=30)
-print(dt)
-print(type(dt))
 
-current_time= time.time()
-current_datetime= datetime.now()
-print(current_datetime)
-print(current_datetime, current_datetime)
+parent = ParentClass()
+child = ChildClass()
+# print(parent)
+# print(child)
 
-d= date(year=2023, month=5, day = 25)
-print(d)
-
-current_date = date.today()
-print(current_date)
+print('7을 더하세요')
+parent.add_num(7)
+child.add_num(7)
+print(parent)
+print(child)
