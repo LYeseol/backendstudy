4ae957277774ce959ce9630b2a5d1426da9b75a6
Yeseol Lee <ashleylee9404@gmail.com>
2023-06-09 14:02:41 +0900

    with open file

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-print("파일 열기")
-file_object = open('example.txt', 'r')
+with open('example.txt', 'r') as file_object:
 
-print("현재파일 위치 확인")
-position = file_object.tell()
-print("Current position: ",position)
-
-print("파일 포인터 위치 변경")
-position=file_object.seek(7)
-
-print("변경된 위치 확인")
-print("New position: ", position)
-
-print('파일 닫기')
-file_object.close()
+    lines = file_object.readlines()
+    for line in lines:
+        print(line.strip())
