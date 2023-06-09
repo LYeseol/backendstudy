923952cec9988c0895b5c9c68f80312ce9423c60
Yeseol Lee <ashleylee9404@gmail.com>
2023-06-09 13:41:54 +0900

    file 위치변경

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-file_object = open('example.txt', 'w')
-content = 'This is a new file. \n Python is fun!'
-file_object.write(content)
+print("파일 열기")
+file_object = open('example.txt', 'r')
+
+print("현재파일 위치 확인")
+position = file_object.tell()
+print("Current position: ",position)
+
+print("파일 포인터 위치 변경")
+position=file_object.seek(7)
+
+print("변경된 위치 확인")
+print("New position: ", position)
+
+print('파일 닫기')
 file_object.close()
