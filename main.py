c5f269cc73848b75a0d92c5e206b13bdd6344689
Yeseol Lee <ashleylee9404@gmail.com>
2023-06-09 14:21:10 +0900

    list file

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-with open('example.txt', 'r') as file_object:
+import os
+#
+# filename= 'example.txt'
+#
+# print('파일이 존재하는지 확인하기')
+# if os.path.isfile(filename):
+#     print(f'{filename}이 존재합니다')
+# else:
+#     print(f'{filename}이 없습니다')
 
-    lines = file_object.readlines()
-    for line in lines:
-        print(line.strip())
+file_object= open('list_example.txt', 'w')
+
+content_list= ["Python", "Java", "C++", "Javascript"]
+for item in content_list:
+    file_object.write(item + '\n')
+
+file_object.close()
