b27cd376f57104c45c4824ffd387d2e823126a85
Yeseol Lee <ashleylee9404@gmail.com>
2023-06-12 10:27:36 +0900

    class practice

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-import os
-current_dir = os.getcwd()
-print(current_dir)
-#os.makedirs('parent_dir/child_dir/grandchild_dir')
-for dirpath, dirnames, filenames in os.walk('parent_dir'):
-    print(f'디렉터리 경로: {dirpath}')
-    print(f'디렉터리 이름: {dirnames}')
-    print(f'파일이름: {filenames}')
 
-#
-# # os.mkdir('new_directory')
-# # os.makedirs('parent_dir/child_dir/grandchild_dir')
-#
-# #change directory
-# os.chdir('new_directory')
-# current_dir2 = os.getcwd()
-# print(current_dir2)
-#
-# with open('example.txt','w') as file_object:
-#     file_object.write('Hello world!')
-#
-# print('done')
-#
-# os.rename('new_directory', 'old_directory')
+
+
+class Car:
+    #class속성
+    wheel =4
+
+    def __init__(self, make, model, color):
+        self.make = make
+        self.model = model
+        self.color = color
+
+    #method
+
+    def drive(self):
+        return 'The car is moving'
+
+    def stop(self):
+        return "The car has stopped"
+
+my_car = Car("Kia", "Morning", "Blue")
+
+print(my_car.make)
+print(my_car.drive())
+print(my_car.stop())
