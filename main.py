5488a159429319aa1c5036ae02ec95a1c00cac1a
Yeseol Lee <ashleylee9404@gmail.com>
2023-06-12 13:04:41 +0900

    package practice

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
+from MyApp.Handlers.text_handler import handle_text
 
-class Vehicle:
-    def __init__(self, make, model, color):
-        self.make = make
-        self.model = model
-        self.color = color
-
-    def start_engine(self):
-        return "The engine is running"
-
-class Car(Vehicle):
-   def start_engine(self):
-       return super().start_engine() + "It's a car engine."
-
-my_car = Car("Kia", "Morning", "Blue")
-
-print(my_car.make)
-print(my_car.start_engine())
+input_text = "python package practice"
+handle_text(input_text)
