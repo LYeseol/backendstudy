772590145e73da5faa927a4b6c576cdbb19f8b07
Yeseol Lee <ashleylee9404@gmail.com>
2023-07-07 13:10:03 +0900

    firebase

--- Django/main.py
+++ Django/main.py
@@ -1,13 +1,30 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from wsgiref.simple_server import make_server
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200, 'OK')
-        self.send_header('Content-Type', 'text/plain')
-        self.end_headers()
-        #web sorket에 파일처럼 쓰는
-        self.wfile.write(b"Hello World")
+def application(envir, start_response):
+    response_body = b"Hello world"
 
-server = HTTPServer(("", 8080), SimpleHTTPRequestHandler)
+    status = "200 Ok"
+    headers =[("Content-Type", "text/plain")]
 
-server.serve_forever()
+    start_response(status, headers)
+    return [response_body]
+
+
+if __name__=='__main__':
+    #server위에 내가 만든 application얹기
+    httpd = make_server("", 8000, application)
+    print("Running....")
+    httpd.serve_forever()
+
+# class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+#     def do_GET(self):
+#         self.send_response(200, 'OK')
+#         self.send_header('Content-Type', 'text/plain')
+#         self.end_headers()
+#         #web sorket에 파일처럼 쓰는
+#         self.wfile.write(b"Hello World")
+#
+# server = HTTPServer(("", 8080), SimpleHTTPRequestHandler)
+#
+# server.serve_forever()
